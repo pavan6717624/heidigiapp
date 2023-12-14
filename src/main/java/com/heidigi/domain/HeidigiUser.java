b8d1eb9b45f656f41a0b669d509030ea1257bfb7
pavan6717624 <pvn.kumar4@gmail.com>
2023-12-14 15:59:26 +0530

    deploy

--- src/main/java/com/heidigi/domain/HeidigiUser.java
+++ src/main/java/com/heidigi/domain/HeidigiUser.java
@@ -3,6 +3,7 @@ package com.heidigi.domain;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -10,16 +11,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "hd_user")
 public class HeidigiUser implements Serializable {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 3956721357336114735L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,7 +36,7 @@ public class HeidigiUser implements Serializable {
 
 	String type = "";
 
-//	@Column(columnDefinition="datetime")
+	@Column(columnDefinition="datetime")
 	Timestamp joinDate;
 
 	@OneToOne(fetch = FetchType.LAZY)
