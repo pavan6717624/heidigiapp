22a7c5ce42adc188b2bbf5d577d85a89537132bd
Pavan <pvn.kumar4@gmail.com>
2024-12-28 17:29:47 +0530

    deply

--- src/main/java/com/heidigi/repository/AmazonProductRepository.java
+++ src/main/java/com/heidigi/repository/AmazonProductRepository.java
@@ -3,12 +3,14 @@ package com.heidigi.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.heidigi.domain.AmazonProduct;
 @Repository
 public interface AmazonProductRepository  extends JpaRepository<AmazonProduct,Long>{
-
+	
+	@Query("select A from AmazonProduct A order by rand()")
 	Optional<AmazonProduct> findByProductUrl(@Param("productUrl") String productUrl);
 }
